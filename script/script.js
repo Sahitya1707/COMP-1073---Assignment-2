5192f8dc9c3c749a352beb57c852277e1c3eeade
Sahitya hp <neupanesahitya1@gmail.com>
2024-03-20 01:19:28 -0400

    added little bit of js to get the value from the fomr

--- script/script.js
+++ script/script.js
@@ -2,18 +2,27 @@
 const nameInput = document.querySelector("#name");
 // slecting flavor
 const flavor = document.querySelector("#flavor");
+console.log(flavor.value);
 
 // selecting all checkbox
 const allCheckboxIngredients = document.querySelectorAll(
   ".form-checkbox input[type='checkbox']"
 );
+
 //selecting text area (instruction box)
 const inputInstruction = document.querySelector("#instruction");
 // selecting submit button
 const submitBtn = document.querySelector(".submit");
 // declaring variable to get the actual value
-let nameValue = nameInput.value;
-console.log(nameValue);
+function emptyForm() {
+  nameInput.value = ``;
+  flavor.value = "";
+  allCheckboxIngredients.forEach((e) => {
+    e.checked = false;
+  });
+}
+
+let additionalIngredients = [];
 
 class Smoothie {
   name;
@@ -31,3 +40,17 @@ class Smoothie {
     let userOrder = `${this.name} want ${this.flavor} smoothie with ${this.ingredients} inside it.`;
   }
 }
+
+// clicking submit button
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log("btn clicked");
+  let nameValue = nameInput.value;
+  let flavorValue = flavor.value;
+  allCheckboxIngredients.forEach((e) => {
+    e.checked === true ? additionalIngredients.push(e.name) : null;
+    console.log(additionalIngredients);
+  });
+  console.log(flavorValue);
+  console.log(nameValue);
+});
